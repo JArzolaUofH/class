03c739976226c77e9dca06bc29d83a352a9771d1
JArzolaUofH <99359606+JArzolaUofH@users.noreply.github.com>
2022-02-09 23:50:10 -0600

    Add files via upload

---
+++ homework_1/lab2.19.py
@@ -0,0 +1,10 @@
+# justine arzola 1804667
+
+l_juice = (int(input("Enter amount of lemon juice (in cups):\n")))
+water = (int(input("Enter amount of water (in cups):\n")))
+agave = (int(input("Enter amount of agave nectar (in cups:\n")))
+servings = (int(input("How many servings does this make?\n")))
+print("Lemon ingredients - yields", '{:.2f}'.format(servings), "servings\n")
+print('{:.2f}'.format(l_juice), "cup(s) lemon juice\n")
+print('{:.2f}'.format(water), "cup(s) water\n")
+print('{:.2f}'.format(agave), "cup(s) agave\n")
