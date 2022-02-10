027e021fa6c47d4e25d82fdff3fef59a5105c344
JArzolaUofH <99359606+JArzolaUofH@users.noreply.github.com>
2022-02-09 23:06:34 -0600

    Add files via upload

---
+++ homework_1/lab1.12new.py
@@ -0,0 +1,11 @@
+#  Justine Arzola 1804667
+
+userNum = int(input())
+if userNum == '':
+    userNum = 0
+
+userNumSquared = userNum * userNum  # Bug here; fix it when instructed
+
+print(userNumSquared)
+
+# Output formatting issue here; fix it when instructed
