8b0ab5921eb00e6e2d99e3e69116e53352ebfc7c
JArzolaUofH <99359606+JArzolaUofH@users.noreply.github.com>
2022-03-09 17:32:18 -0600

    Add files via upload

---
+++ homework_2/lab6.17.py
@@ -0,0 +1,22 @@
+userpassword = input("please enter password:")
+result = ''
+
+i = 0
+while i < len(userpassword):
+    new = userpassword[i]
+    if new == 'i':
+        result += '!'
+    elif new == 'a':
+        result += '@'
+    elif new == 'm':
+        result += 'M'
+    elif new == 'B':
+        result += '8'
+    elif new == 'o':
+        result += '.'
+    else:
+        result += new
+    i += 1
+
+result += "q*s"
+print(result)
