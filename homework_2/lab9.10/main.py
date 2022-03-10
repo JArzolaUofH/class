0625a79809307d499fa95f3047cae733995d2f12
JArzolaUofH <99359606+JArzolaUofH@users.noreply.github.com>
2022-03-09 21:13:50 -0600

    Add files via upload

---
+++ homework_2/lab9.10/main.py
@@ -0,0 +1,18 @@
+import csv
+
+file = input()
+f = open(file)
+info = csv.reader(f, delimiter=',')
+contents = []
+for row in info:
+    for content in row:
+        contents.append(content.strip())
+
+for i in range(len(contents)):
+    if contents[i] not in contents[:i]:
+        count = 0
+        for c in contents:
+            if contents[i] == c:
+                count += 1
+        print(contents[i], count)
+f.close()
